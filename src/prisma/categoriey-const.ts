ca1902161904e174ab119cb198f5a8b3678410c1
Noldor99 <qwerr19942017@gmail.com>
2026-03-11 21:06:10 +0200

    fix build

--- src/prisma/categoriey-const.ts
+++ src/prisma/categoriey-const.ts
@@ -1,28 +1,9 @@
 export const categories_data = [
-  {
-    slug: "laptops",
-    ua: "Ноутбуки",
-    en: "Laptops",
-  },
+
   {
     slug: "monitors",
     ua: "Монітори",
     en: "Monitors",
-  },
-  {
-    slug: "keyboards",
-    ua: "Клавіатури",
-    en: "Keyboards",
-  },
-  {
-    slug: "mice",
-    ua: "Миші",
-    en: "Mice",
-  },
-  {
-    slug: "headphones",
-    ua: "Навушники",
-    en: "Headphones",
   },
   {
     slug: "tablets",
